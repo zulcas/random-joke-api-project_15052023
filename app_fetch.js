2c37323ed4d2bb3506b53e190fe25128709ebb94
oskytar <oscar.miras@gmail.com>
2020-12-31 11:53:22 +0100

    use of fetch api, promise and async version

--- app_fetch.js
+++ app_fetch.js
@@ -5,17 +5,19 @@ const button = document.querySelector('#get-joke');
 
 button.addEventListener('click', function(){
     getRandomJoke();
-    console.log(jokeDIV.innerHTML)
 })
 
-// El código asíncrono puede ser gestionado dentro de una función con la directiva 'async'
-async function getRandomJoke(){
+function getRandomJoke(){
     const url = 'https://api.chucknorris.io/jokes/random'
+ 
+    fetch(url).
+    then(response => response.json()).
+    then(data => updateDOM(data)).
+    catch(error => console.log(error))
 
-    // la directiva 'await' espera a que se acabe la función asíncrona invocada. Hasta que no se acabe, no se ejecutará la siguiente instrucción
-    let response = await fetch(url)
-    let data = await response.json()
-    updateDOM(data)
+    // Fijaos que esto se ejecuta antes de recibir la respuesta del fetch!!
+    console.log(jokeDIV.textContent)
+
 }
 
 function updateDOM(data) {
